f4b0976e269a6c0f0c0becde67c20f548eb9a9bf
Sushant Singh <sushant22007@gmail.com>
2025-07-26 20:26:16 +0530

    Add programs to print inverted star pattern and star pattern

---
+++ Pattern/invertedStarPattern.java
@@ -0,0 +1,17 @@
+package Pattern;
+import java.util.Scanner;
+
+public class invertedStarPattern {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        for (int line = 1; line <= n; line++){
+            for(int star = 1; star <= n -line+1; star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
