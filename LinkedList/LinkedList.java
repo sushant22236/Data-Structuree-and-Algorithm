26fb076d563172ff721edad2d0dd631cd618b008
Sushant Singh <sushant22007@gmail.com>
2025-12-11 16:15:10 +0530

    Enhance LinkedList class: improve addFirst and addlast methods to handle empty list case; add print method for better output visibility; create addInTheMiddleOfLinkedList class as a placeholder for future implementation.

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -14,22 +14,55 @@ public class LinkedList {
     public static Node tail;
 
     public void addFirst(int data){
+        //step 1. -> create new node
         Node newNode = new Node(data);
         if(head == null){
             head = tail = newNode;
+            return;
         }
+        //step 2. -> newNode next = head
         newNode.next = head;
+        //step 3. -> head = newNode
         head = newNode;
     }
 
     public void addlast(int data){
         Node newNode = new Node(data);
+        if(head == null ){
+            head = tail = newNode;
+            return;
+        }
         tail.next = newNode;
         tail = newNode;
     }
+
+    public void print(){
+        if(head == null){
+            System.out.println("Linkedlist is empty");
+            return;
+        }
+
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.println();
+
+    }
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
-        ll.addFirst(1);
+        // ll.head = new Node(1);
+        // ll.head.next = new Node(2);
+        // ll.head.next = new Node(3);
+        ll.print();
         ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addlast(3);
+        ll.print();
+        ll.addlast(4);
+        ll.print();
     }
 }
---
+++ LinkedList/addInTheMiddleOfLinkedList.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class addInTheMiddleOfLinkedList {
+
+    public void addInTheMiddle(int data){
+
+    }
+    public static void main(String args[]){
+
+    }
+}
