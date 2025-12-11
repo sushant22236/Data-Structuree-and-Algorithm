ddbd035abdfa52bf934a4676fd9d510a60206a09
Sushant Singh <sushant22007@gmail.com>
2025-12-11 16:38:51 +0530

    Refactor addInTheMiddleOfLinkedList class: update addInTheMiddle method to accept index parameter; restructure Node class and head declaration for better clarity and functionality.

--- LinkedList/addInTheMiddleOfLinkedList.java
+++ LinkedList/addInTheMiddleOfLinkedList.java
@@ -2,8 +2,29 @@ package LinkedList;
 
 public class addInTheMiddleOfLinkedList {
 
-    public void addInTheMiddle(int data){
+    public static class Node{
+        int data;
+        Node next;
 
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+
+    public void addInTheMiddle(int idx, int data){
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while(i < idx-1){
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
     }
     public static void main(String args[]){
 
