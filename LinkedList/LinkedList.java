4107a6928195084d6827e3c1e0c1f87b13eea845
Sushant Singh <sushant22007@gmail.com>
2025-12-15 15:29:02 +0530

    Implement checkPalindrome method in LinkedList class to verify if the list is a palindrome.

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -139,6 +139,39 @@ public class LinkedList {
         }
         return slow; //slow is my midNode
     }
+
+    public boolean checkPalindrome(){
+        if(head == null || head.next != null){
+            return true;
+        }
+        //step .1 find mid of linkedlist
+        Node mid = findMid(head);
+
+        // step .2 reverse the second half 
+        Node prev = null;
+        Node curr = mid;
+        Node next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+
+        // step .3 check if 1st half == 2half
+
+        while(right != null){
+            if(left.data != right.data){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+
+    }
     public static void main(String args[]){
         LinkedList ll = new LinkedList();
         // ll.head = new Node(1);
