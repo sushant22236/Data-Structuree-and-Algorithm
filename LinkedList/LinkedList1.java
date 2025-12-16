43f983102803d78dd3a62600683c1ff785f67269
Sushant Singh <sushant22007@gmail.com>
2025-12-16 14:36:04 +0530

    Implement cycle detection and removal in LinkedList1 class; add classroom class for LinkedList demonstration.

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -42,16 +42,54 @@ public class LinkedList1 {
         }
         return false;
     }
+
+    public static void removeCycle(){
+        //step 1. Detect cycle
+
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next; //+1
+            fast = fast.next.next;  //+2
+            if(slow == fast){
+                cycle = true;
+                break;
+            }
+        }
+
+        if(cycle == false){
+            return;
+        }
+
+        //step 2. Find meeting point
+        
+        slow = head;
+        Node prev = null;
+        while(slow != fast){
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        //step 3. remove cycle last.next = null
+        prev.next = null;
+
+    }
     public static void main(String args[]){
         //LinkedList1 ll = new LinkedList1();
         head = new Node(1);
         head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next = head; 
+        Node temp = new Node(3);
+        head.next.next = temp;
+        head.next.next.next = new Node(5);
+        head.next.next.next.next = temp; 
         
         // ll.print();
 
+        System.out.println(isCyclic());
+        removeCycle();
         System.out.println(isCyclic());
 
     }
---
+++ LinkedList/classroom.java
@@ -0,0 +1,22 @@
+package LinkedList;
+import java.util.LinkedList;
+
+public class classroom {
+    public static void main(String args[]){
+
+        //create
+        LinkedList<Integer> ll = new LinkedList<>();
+        
+        //add
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        System.out.println(ll);
+
+        //remove
+        ll.removeFirst();
+        ll.removeLast();
+
+        System.out.println(ll);
+    }
+}
